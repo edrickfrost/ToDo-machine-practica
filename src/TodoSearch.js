6e16f268eee31bff94dc9021c025a63d5bf8ff8b
edrickfrost <edrickfrost@gmail.com>
2022-06-23 21:35:46 -0500

    implementado el buscador de ToDos

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,21 +1,21 @@
 import React from "react";
 import './TodoSearch.css';
 
-function TodoSearch(){
-    const [searchValue, setSearchValue] = React.useState('');
+function TodoSearch({searchValue, setSearchValue}){
+    // const [searchValue, setSearchValue] = React.useState('');
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
         setSearchValue(event.target.value);
     };
-    return [
+    return (
         <input
             className="TodoSearch"
             placeholder="cebolla"
             value={searchValue}
             onChange={onSearchValueChange}
-        />,
-        <p>{searchValue}</p>
-    ];
+        />
+        // <p>{searchValue}</p>
+    );
 }
 
 export { TodoSearch };
